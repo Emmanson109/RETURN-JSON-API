4f3e9f1a4d773637bd4e72d7fb28062a3d619c4a
Emmanson <Emmanson109@gmail.com>
2025-01-31 20:14:17 +0100

    another commit to add a response code

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req,res) => {
-    res.json({
+    res.status(200).json({
         email: 'Emmanson109@gmail.com',
         currentDateTime: new Date().toISOString(),
         github_url: 'https://github.com/Emmanson109/RETURN-JSON-API.git'
